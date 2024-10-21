45050861e5247f884af7b738a25e693c30f01b28
Avvai Aravind <avvaiaravind@gmail.com>
2024-10-21 22:14:53 +0530

    created dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./style.css"; /*  for importing style.css */
+
+const dropDownBtn = document.getElementById("dropdown-btn");
+const dropDownMenu = document.getElementById("dropdown-menu");
+const toggleArrow = document.querySelector(".fa-caret-up");
+function toggleDropdown() {
+  dropDownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow-down");
+}
+
+dropDownBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (dropDownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
